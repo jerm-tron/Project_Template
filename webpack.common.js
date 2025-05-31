82759e22927097346925b83e83da426ebe3c268d
Jeremy <jerm.tron@gmail.com>
2025-05-31 15:16:04 -0400

    Webpack merge configured

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
   devtool: "eval-source-map",
   devServer: {
     watchFiles: ["./src/template.html"],
--- webpack.prod.js
+++ webpack.prod.js
@@ -3,13 +3,4 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
 });
